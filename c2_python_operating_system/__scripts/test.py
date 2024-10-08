d9107b8d994f06756d4f5edb25de867de0d24d7d
HypertextAssassin0273 <k190273@nu.edu.pk>
2024-10-08 16:55:12 +0500

    new file added in scripts

---
+++ c2_python_operating_system/__scripts/test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from __modules import areas
+
+print("area of triangle:", areas.triangle(10, 5))
+print("area of rectangle:", areas.rectangle(10, 5))
+print(f"area of circle: {areas.circle(4):.2f}")
