6be56cff3ec909767ed9816acec0efb7ad3e23c4
HypertextAssassin0273 <k190273@nu.edu.pk>
2024-10-08 17:39:28 +0500

    another example added for modules usage

--- c2_python_operating_system/__scripts/__modules/areas.py
+++ c2_python_operating_system/__scripts/__modules/areas.py
@@ -9,6 +9,9 @@ def rectangle(width, height):
 def circle(radius):
     return math.pi * radius ** 2
 
+def doughnut(outer_radius, inner_radius):
+    return circle(outer_radius) - circle(inner_radius)
+
 # notes:
 # 1)  this is a file to be used as a module
 # 2)  such files do not contain any executable code (i.e. only definitions, classes, functions, etc)
--- c2_python_operating_system/__scripts/test.py
+++ c2_python_operating_system/__scripts/test.py
@@ -5,3 +5,4 @@ from __modules import areas
 print("area of triangle:", areas.triangle(10, 5))
 print("area of rectangle:", areas.rectangle(10, 5))
 print(f"area of circle: {areas.circle(4):.2f}")
+print(f"area of doughnut: {areas.doughnut(8, 4):.2f}")
