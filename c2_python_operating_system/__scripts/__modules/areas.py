74d0d77efc63c0caecf9c6a50e4f8c53c3f28754
HypertextAssassin0273 <k190273@nu.edu.pk>
2024-10-08 16:49:54 +0500

    new module added in scripts

---
+++ c2_python_operating_system/__scripts/__modules/areas.py
@@ -0,0 +1,14 @@
+import math
+
+def triangle(base, height):
+    return base * height / 2
+
+def rectangle(width, height):
+    return width * height
+
+def circle(radius):
+    return math.pi * radius ** 2
+
+# notes:
+# 1)  this is a file to be used as a module
+# 2)  such files do not contain any executable code (i.e. only definitions, classes, functions, etc)
