ff8207a013f7dc0bc3604ec5db0b567abbf0fcc8
HypertextAssassin0273 <k190273@nu.edu.pk>
2024-10-28 03:59:08 +0500

    code logic modified

--- c2_python_operating_system/__scripts/processing_log_files/check_cron_user.py
+++ c2_python_operating_system/__scripts/processing_log_files/check_cron_user.py
@@ -2,12 +2,17 @@
 
 import sys, re
 
+usernames = {}
 logfile = sys.argv[1]
+
 with open(logfile) as file:
-    pattern = r"CRON.+USER \((.+)\)$"
+    pattern = r"CRON.+USER \((.+)\)$" # find only CRON users & capture their names from the log
     for line in file:
         result = re.search(pattern, line)
         if result is not None:
-            print(result[1])
+            name = result[1]
+            usernames[name] = usernames.get(name, 0) + 1 # count the number of times user appears in log
+
+print(usernames)
 
 # usage: ./check_cron_user.py ../../__assets/syslog
