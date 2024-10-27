5574054b1a709d5d676203a058ee117e06dd3c12
HypertextAssassin0273 <k190273@nu.edu.pk>
2024-10-28 02:21:01 +0500

    new files added for 'proccessing log_files'

---
+++ c2_python_operating_system/__scripts/processing_log_files/check_cron_user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import sys, re
+
+logfile = sys.argv[1]
+with open(logfile) as file:
+    pattern = r"CRON.+USER \((.+)\)$" 
+    for line in file:
+        result = re.search(pattern, line)
+        if result is not None:
+            print(result[1])
+
+# usage: ./check_cron_user.py ../../__assets/syslog
