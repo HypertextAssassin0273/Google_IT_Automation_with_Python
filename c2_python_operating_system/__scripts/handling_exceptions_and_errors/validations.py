eb1e702ce32463ae4abf3dbe792981eb0d88a421
HypertextAssassin0273 <k190273@nu.edu.pk>
2024-11-19 20:30:06 +0500

    try-except block added with test cases

--- c2_python_operating_system/__scripts/handling_exceptions_and_errors/validations.py
+++ c2_python_operating_system/__scripts/handling_exceptions_and_errors/validations.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
 
 def validate_user(username, minlen):
-    assert type(username) == str, "username must be a string"
+    assert type(username) == str, "Error: username must be a string"
+
     if minlen < 1:
-        raise ValueError("minlen must be at least 1")
+        raise ValueError("Error: min. length must be at least 1")
     if len(username) < minlen:
         return False
     if not username.isalnum():
         return False
+    
     return True
+
+
+# test-cases:
+try:
+    validate_user("", -1)         # fail
+    # validate_user("", 1)          # pass
+    # validate_user("myuser", 1)    # pass
+    # validate_user(88, 1)          # fail
+    # validate_user([], 1)          # fail
+    # validate_user(["name"], 1)    # fail
+
+except Exception as e: # catches all exceptions
+    print(e)
