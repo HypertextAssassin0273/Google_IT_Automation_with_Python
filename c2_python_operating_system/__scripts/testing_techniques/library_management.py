9aa0f78dbad23aa2b5a4f886b1c7516f047ee86b
HypertextAssassin0273 <k190273@nu.edu.pk>
2024-11-19 16:58:06 +0500

    files added for new example

---
+++ c2_python_operating_system/__scripts/testing_techniques/library_management.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+class Library:
+    def __init__(self):
+        self.collection = []
+    
+    def add_book(self, book_title):
+        self.collection.append(book_title)
+    
+    def has_book(self, book_title):
+        return book_title in self.collection
---
+++ c2_python_operating_system/__scripts/testing_techniques/library_management_test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import unittest
+from library_management import Library
+
+# Unit test for the Library system to add a book (using AAA design pattern)
+class TestLibrary(unittest.TestCase):
+    def test_adding_book_to_library(self):
+        # Arrange
+        library = Library()
+        new_book = "Python Design Patterns"
+
+        # Act
+        library.add_book(new_book)
+
+        # Assert
+        self.assertTrue(library.has_book(new_book))
+
+# Running the test
+library_test_output = unittest.TextTestRunner().run(unittest.TestLoader().loadTestsFromTestCase(TestLibrary))
+print(library_test_output)
