ca9986789e9624fe80762afeed00ca5c41345c1c
HypertextAssassin0273 <k190273@nu.edu.pk>
2024-12-15 22:44:13 +0500

    comment added for describing the functionality

--- c2_python_operating_system/__scripts/py/for_bash_scripting/capitalize.py
+++ c2_python_operating_system/__scripts/py/for_bash_scripting/capitalize.py
@@ -4,3 +4,6 @@ import sys
 
 for line in sys.stdin:
     print(line.strip().capitalize())
+
+
+# NOTE: it capitalizes the first letter of each word, and lowercase the rest in each line of the input.
