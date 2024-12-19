fab315b1c06c8e5c7b09f49049df72d2c8c00b69
HypertextAssassin0273 <k190273@nu.edu.pk>
2024-12-19 07:01:38 +0500

    logic improvised

--- c2_python_operating_system/__scripts/advance/retry.sh
+++ c2_python_operating_system/__scripts/advance/retry.sh
@@ -2,11 +2,16 @@
 
 n=0
 command=$1
-while ! $command && [ $n -le 5 ]; do
-        sleep $n
-        ((n+=1))
-        echo "Retry #$n"
-done;
+
+while ! $command; do
+    if [ $n -ge 5 ]; then
+        echo "The command has failed after $n attempts."
+        exit 1
+    fi
+    sleep $n
+    ((n+=1))
+    echo "Retry #$n"
+done
 
 
 # USAGE: ./retry.sh <command>
