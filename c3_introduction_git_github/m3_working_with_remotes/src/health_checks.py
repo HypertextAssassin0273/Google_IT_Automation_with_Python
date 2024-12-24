1c367289c8a2e6c5de7ad6bda82514ddb38a6fe6
HypertextAssassin0273 <k190273@nu.edu.pk>
2024-12-24 07:47:39 +0500

    more versions added for all_checks.py script for consistency

--- c3_introduction_git_github/m3_working_with_remotes/src/health_checks.py
+++ c3_introduction_git_github/m3_working_with_remotes/src/health_checks.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import os, shutil, sys
+import os, shutil, sys, socket, psutil
 
 def check_reboot():
     """Returns True if the computer has a pending reboot."""
@@ -19,12 +19,26 @@ def check_disk_full(disk, min_gb, min_percent):
 
 def check_root_full():
     """Returns True if the root partition is full, False otherwise."""
-    return check_disk_full(disk="/",min_gb=2,min_percent=10)
+    return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
+def check_cpu_constrained():
+    """Returns True if the cpu is having too much usage, False otherwise."""
+    return psutil.cpu_percent(1) > 75
+
+def check_no_network():
+    """Returns True if it fails to resolve Google's URL, False otherwise."""
+    try:
+        socket.gethostbyname("google.com")
+        return False
+    except:
+        return True
 
 def main():
     checks = [
-        (check_reboot, "Pending Reboot"),
-        (check_root_full, "Root partition full")
+        (check_reboot, "Pending Reboot."),
+        (check_root_full, "Root partition full"),
+        (check_cpu_constrained, "CPU load too high."),
+        (check_no_network, "No working network.")
     ]
     everything_ok = True
 
