0c2bde61c8a57bf670d3b8d59c588bfc14a83b24
HypertextAssassin0273 <k190273@nu.edu.pk>
2024-12-27 12:20:58 +0500

    video title and comments modified

--- c3_introduction_git_github/m3_working_with_remotes/src/L1_scripts.sh
+++ c3_introduction_git_github/m3_working_with_remotes/src/L1_scripts.sh
@@ -1,9 +1,8 @@
-# NOTE: in all scripts, we are using 'code' as the text editor. You can use any text editor of your choice like 'atom', 'vim', 'gedit', etc
-
-# Video: Basic Interaction with GitHub
+## Video: Basic Interaction with GitHub ##
 git clone <url> # get local copy to your computer
-code README.md # create readme file (in VS Code editor) 
+code README.md # create readme file (in VS Code editor)
+# NOTE: here you can use any other text editor (like 'atom', 'gedit', 'nano', etc)
 git add -a -m "Add one more line to README.md"
 git push # push modified README.md to GitHub
-git config --global credential.helper cache # caches our credentials for a time window so that we don't need to enter our password with every interaction
+git config --global credential.helper cache # caches our credentials for a time window (so we don't need to enter our password with every interaction)
 git pull # retrieve new changes from the repository
