7a45510e7d7eacb655d6670cbc74a1aa2033157d
HypertextAssassin0273 <k190273@nu.edu.pk>
2024-12-27 13:58:31 +0500

    new script lesson added

---
+++ c3_introduction_git_github/m4_collaboration/src/L3_scripts.sh
@@ -0,0 +1,23 @@
+## Video: Tracking Issues ##
+cd health-checks/
+
+code README.md
+# this will open the contents of README.md file in VS Code editor:
+## health-checks project
+This repo will be populated with lots of fancy checks. 
+
+Currently the main script is health_checks.py
+
+This script will print "Everything ok" if all checks pass, 
+or the corresponding error messages if some checks fail.
+
+git commit -a
+# this will prompt you to write the commit message (in your default editor):
+Update README to use the new name of the script
+
+Also add more information about how this works. 
+Closes #1
+(...)
+# NOTE: "Closes #1" in the commit message will automatically close the issue on GitHub
+
+git push
