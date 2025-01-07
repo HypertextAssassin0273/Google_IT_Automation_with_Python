20b2a95a68d3716d094e47db473c7c199000f1fe
HypertextAssassin0273 <k190273@nu.edu.pk>
2025-01-07 20:07:39 +0500

    app logic modified & comments added

--- c4_troubleshooting_debugging_techniques/m1_troubleshooting_concepts/src/send_reminders.py
+++ c4_troubleshooting_debugging_techniques/m1_troubleshooting_concepts/src/send_reminders.py
@@ -1,59 +1,52 @@
 #!/usr/bin/env python3
-# Code from Coursera
 
-# To trigger the error, LANG=en_US, UTF-8
-
-import datetime
-import email
-import smtplib
-import sys
+import datetime, email, smtplib, sys
 
 def usage():
-    print("send_reminders: Send meeting reminders")
-    print()
-    print("invocation:")
-    print("    send_reminders 'date|Meeting|Emails' ")
+    print('usage:')
+    print('1) python3 send_reminders.py "Date|Meeting Title|Emails"')
+    print('2) ./send_reminders.py "Date|Meeting Title|Emails"')
     return 1
 
 def dow(date):
-    dateobj = datetime.datetime.strptime(date, r"%Y-%m-%d")
+    dateobj = datetime.datetime.strptime(date, r"%Y-%m-%d") # NOTE: format changed to keep consistency with the input
     return dateobj.strftime("%A")
 
 def message_template(date, title):
     message = email.message.EmailMessage()
     weekday = dow(date)
     message['Subject'] = f'Meeting reminder: "{title}"'
-    message.set_content(f'''
-Hi all!
-    
-This is a quick mail to remind you all that we have a meeting about: "{title}""
-the {weekday} {date}
-
-See you there.
-''')
+    message.set_content(
+        f"Hi all!\n\n"
+        f"This is a quick mail to remind you all that we have a meeting about: \"{title}\"\n"
+        f"the {weekday} {date}\n\n"
+        f"See you there.\n"
+    )
     return message
 
 def send_message(message, emails):
-    smtp = smtplib.SMTP('localhost')
+    smtp = smtplib.SMTP('localhost', 1025) # NOTE: using debugging server to test emails (since SMTP server is not running on localhost)
     message['From'] = 'noreply@example.com'
     for email in emails.split(','):
-        del message['To']
         message['To'] = email
         smtp.send_message(message)
+        del message['To'] # NOTE: instead of clearing 'To' field before sending the email, it is cleared afterwards to avoid errors
     smtp.quit()
-    pass
 
 def main():
     if len(sys.argv) < 2:
         return usage()
 
     try:
-        date, time, emails = sys.argv[1].split('|')
-        message = message_template(date, time)
+        date, title, emails = sys.argv[1].split('|')
+        message = message_template(date, title)
         send_message(message, emails)
         print("Successfully sent reminders to:", emails)
     except Exception as e:
-        print("Failure to send email with: {}".format(e), file=sys.stderr)
+        print("Failure to send email with: {}".format(e), file=sys.stderr) # NOTE: error message now gives more context about the failure
 
 if __name__ == '__main__':
     sys.exit(main())
+
+
+# SIDE-NOTE: use command 'python3 -m smtpd -c DebuggingServer -n localhost:1025' to start a debugging server before running this script
